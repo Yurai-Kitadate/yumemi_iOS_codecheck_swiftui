e1bd9e886c2e6f1d454e7b2cd9a035d09ecdd459
Yurai-Kitadate <yuurai18@gmail.com>
2023-03-31 22:54:14 +0900

    Initial Commit

---
+++ GitHubRepositoryViewer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubRepositoryViewer
+//
+//  Created by 北舘友頼 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepositoryViewer/GitHubRepositoryViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoryViewerApp.swift
+//  GitHubRepositoryViewer
+//
+//  Created by 北舘友頼 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoryViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
